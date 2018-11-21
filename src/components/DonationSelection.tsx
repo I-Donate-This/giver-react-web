1c39314d675e2bb661c5c42f3a053e69eb840622
333737229 <peter.yu@rbc.com>
2018-11-20 22:28:47 -0500

    style the buttons

--- src/components/DonationSelection.tsx
+++ src/components/DonationSelection.tsx
@@ -1,11 +1,19 @@
 import * as React from 'react';
 import Button from "@material-ui/core/Button/Button";
 
+
+const buttonContainerStyle = {display: 'flex', justifyContent: 'center', alignItems: 'center', padding: 30};
 const DonationSelection = () =>
     <div>
-        <div><Button>Time</Button></div>
-        <div><Button>Money</Button></div>
-        <div><Button>Item</Button></div>
+        <div style={buttonContainerStyle}>
+            <Button size="large" variant="outlined">Time</Button>
+        </div>
+        <div style={buttonContainerStyle}>
+            <Button size="large" variant="outlined">Money</Button>
+        </div>
+        <div style={buttonContainerStyle}>
+            <Button size="large" variant="outlined">Item</Button>
+        </div>
     </div>;
 
 export default DonationSelection;
